69cbe2c84af344c8024f862c889c5966000d67e7
ihatetmat <kchk0628@naver.com>
2023-10-12 14:28:13 +0900

    굳굳

---
+++ src/main/java/com/DM/DeveloperMatching/dto/User/UserRequestDto.java
@@ -0,0 +1,19 @@
+package com.DM.DeveloperMatching.dto.User;
+
+import com.DM.DeveloperMatching.domain.Level;
+import com.DM.DeveloperMatching.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserRequestDto {
+    private String userName;
+    private String part;
+    private Level level;
+    private String introduction;
+    private String tech;
+    private String career;
+}
---
+++ src/main/java/com/DM/DeveloperMatching/repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.DM.DeveloperMatching.repository;
+
+import com.DM.DeveloperMatching.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
+
