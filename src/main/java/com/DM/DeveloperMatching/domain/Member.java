46ea988a1d0a625a78854b815ba46bab98ae324a
Jeongho <fgh2585@gmail.com>
2023-10-12 12:15:09 +0900

    Member like -> liked Review rId 오타 수정

--- src/main/java/com/DM/DeveloperMatching/domain/Member.java
+++ src/main/java/com/DM/DeveloperMatching/domain/Member.java
@@ -16,8 +16,8 @@ public class Member {
     @Column(name = "member_status")
     private MemberStatus memberStatus;
 
-    @Column(name = "like")
-    private boolean like;
+    @Column(name = "liked")
+    private boolean liked;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
